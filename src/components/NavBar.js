dba967565c0347c6958cf91454eed67908507684
Proyecto_con_JS <veronicavipputo@gmail.com>
2022-03-20 00:14:13 -0300

    20220320 00:14

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
                 <Link className='navbar-brand' to='/'>
-                    <img src="/atom.png" alt="" width="50" height="50"></img>
+                    <img src="/atom.png" alt="" width="60" height="60"></img>
                 </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
