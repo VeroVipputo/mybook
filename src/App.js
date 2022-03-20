b90dc471370920ca004e24e2d0cfbb6fb0c328cd
Proyecto_con_JS <veronicavipputo@gmail.com>
2022-03-19 21:51:43 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>Hola Mundo</div>
+    <div className='App'>
+      <Router>
+        <NavBar/>
+      </Router>
+    </div>
   );
 }
 
