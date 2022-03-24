c7c017860ed5a81006f8ea3d7981975f13cb96e5
Diego Tettamanti <veronicavipputo@gmail.com>
2022-03-23 21:37:41 -0300

    nuevoCommit

---
+++ src/components/pages/itemList.js
@@ -0,0 +1,41 @@
+
+export const elementos = [
+    {
+    id: 1,
+    producto:'Samsung',
+    precio: 1000
+    },
+    {
+    id: 2,
+    producto:'Lg',
+    precio: 1500
+    },
+    {
+    id: 3,
+    producto:'Sony',
+    precio: 1750
+    },
+    {
+    id: 4,
+    producto:'Philips',
+    precio: 750
+    },
+    {
+     id: 5,
+     producto:'Panasonic',
+     precio: 1350
+    }    
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
