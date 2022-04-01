13afee6db27f2056d01d429d79fc2a982e8fc345
Diego Tettamanti <veronicavipputo@gmail.com>
2022-03-31 21:41:55 -0300

    Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,16 @@
+import React from "react";
 
 
+const Books = (libro) => {
 
-const Books = () => {
-    return (  );
+    return ( 
+            <>
+                <h3> { libro }</h3>
+                <p>Breve descripcion</p>
+                <h4>Precio</h4>
+            
+            </>
+     );
 }
  
 export default Books;
